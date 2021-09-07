e4460cad03cdd082d236cbce7a331cc799127f2a
ONGAMER0510 <ojasvnikamoo7@gmail.com>
2021-09-07 11:05:33 +0530

    contact us

--- the-apple/miney.py
+++
@@ -1,9 +0,0 @@
-import keyboard, os
-while True:
-    if keyboard.is_pressed("ctrl+t"):
-        try:
-            os.system("taskkill /IM 'javaw.exe' /F")
-        except:
-            pass
-    else:
-        pass
